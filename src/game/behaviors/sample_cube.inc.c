82563e68b1e48b7c571f5206da9e7d78480dadd2
arthurtilly <arthurtilly413@gmail.com>
2024-03-01 19:35:44 +1300

    fixed quats

--- src/game/behaviors/sample_cube.inc.c
+++ src/game/behaviors/sample_cube.inc.c
@@ -265,8 +265,8 @@ void bhv_sample_cube_init(void) {
 void bhv_sample_cube_loop(void) {
 
     if (obj_has_model(o, MODEL_M_ARM_L)) {
-        Vec3f euler = {0.0f, 0.65f, 0.0f};
-        Quat q = {0.0f, 0.0f, 0.0f, 1.0f};
+        Vec3f euler = {-1.f, 0.3f, -0.4f};
+        Quat q;
 
         print_text_fmt_int(20, 100, "%d", (int) (euler[0] * 100));
         print_text_fmt_int(100, 100, "%d", (int) (euler[1] * 100));
@@ -284,22 +284,6 @@ void bhv_sample_cube_loop(void) {
         print_text_fmt_int(20, 60, "%d", (int) (euler[0] * 100));
         print_text_fmt_int(100, 60, "%d", (int) (euler[1] * 100));
         print_text_fmt_int(180, 60, "%d", (int) (euler[2] * 100));
-        
-
-        /*
-
-        quat_to_euler(o->rigidBody->angleQuat, euler);
-        print_text_fmt_int(20, 100, "%d", (int) (o->rigidBody->angleQuat[0] * 100));
-        print_text_fmt_int(100, 100, "%d", (int) (o->rigidBody->angleQuat[1] * 100));
-        print_text_fmt_int(180, 100, "%d", (int) (o->rigidBody->angleQuat[2] * 100));
-
-        euler_to_quat(euler, o->rigidBody->angleQuat);
-
-        print_text_fmt_int(20, 70, "%d", (int) (o->rigidBody->angleQuat[0] * 100));
-        print_text_fmt_int(100, 70, "%d", (int) (o->rigidBody->angleQuat[1] * 100));
-        print_text_fmt_int(180, 70, "%d", (int) (o->rigidBody->angleQuat[2] * 100));
-
-        */
     }
 
     if ((o->rigidBody->parentBody && o->rigidBody->parentBody->asleep == 0) || o->oTimer < 10) {
